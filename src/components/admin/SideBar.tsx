322c8914f492785ccc1b73db896e790dc0d4979b
Vanlq162003 <113500569+Vanlq162003@users.noreply.github.com>
2023-07-23 21:08:37 +0700

    base

---
+++ src/components/admin/SideBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Nav } from "react-bootstrap";
+
+
+type Props = {}
+
+const SideBar = (props: Props) => {
+    return <>
+        <div>this is Sidebar</div>
+
+    </>
+}
+
+export default SideBar
---
+++ src/layouts/AdminLayout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+
+import SideBar from '../components/admin/SideBar';
+
+type Props = {}
+
+const AdminLayout = (props: Props) => {
+  return <>
+   
+    <SideBar  />
+    
+  </>
+}
+
+export default AdminLayout
